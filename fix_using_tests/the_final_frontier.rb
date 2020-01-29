34b926580baf986c3bfebd2befd75d5723328c52
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-29 18:39:58 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,8 +12,10 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(crew)
+  greetings = []
+  crew.each {|crew_member| greetings << "Hello #{crew_member}."}
+  greetings
 end
 
 def engage
